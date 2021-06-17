7921dad17f956dc1e0bd7cd66001a963725b31ce
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2021-06-17 18:27:12 +0200

    Update after.js

--- after.js
+++ after.js
@@ -25,16 +25,8 @@ function after(n, func) {
    if (typeof func !== 'function') {
     throw new TypeError('Expected a function')
   }
-   if (typeof func !== 'function') {
-    throw new TypeError('Expected a function')
-  }
-   if (typeof func !== 'function') {
-    throw new TypeError('Expected a function')
-  }
   
   const rand = Math.random();
-  const rand2 = Math.random();
-  const rand3 = Math.random();
   n = n || 0
   return function(...args) {
     if (--n < 1) {
